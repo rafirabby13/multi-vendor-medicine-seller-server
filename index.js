0f88572e75f27ed6460e23a8375dd990a8a88181
rafi_ahmed <rafiiitju282@gmail.com>
2025-01-14 21:56:26 +0600

    added cart data post and get by query

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bbiovs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -18,23 +16,44 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-   
+    const cartCollection = client.db("medicineSelling").collection("cart");
+
+    // Cart routes
+
+    app.post("/cart", async (req, res) => {
+      const cartData = req.body;
+      console.log(cartData);
+      const result = await cartCollection.insertOne(cartData);
+      res.send(result);
+    });
+    app.get("/cart", async (req, res) => {
+      const email = req.query.email;
+
+      const query = {
+        email: email,
+      };
+
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
     
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+     
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Medicine vendor selling medicines...haha");
