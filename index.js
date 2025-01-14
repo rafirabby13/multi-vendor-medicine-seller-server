fdaab22ed60a0c68e1f3a29d740ad6a8f68f3048
rafi_ahmed <rafiiitju282@gmail.com>
2025-01-15 00:51:07 +0600

    added payment method server side

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
+
 require("dotenv").config();
 const cors = require("cors");
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+
 app.use(cors());
 app.use(express.json());
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bbiovs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,7 +30,7 @@ async function run() {
 
     app.post("/cart", async (req, res) => {
       const cartData = req.body;
-      console.log(cartData);
+    //   console.log(cartData);
       const result = await cartCollection.insertOne(cartData);
       res.send(result);
     });
@@ -88,6 +91,24 @@ async function run() {
         res.send(result);
       });
 
+
+    //   payment api 
+
+    app.post('/create-payment-intent', async (req, res) => {
+        
+        const {price} = req.body
+        const amount = parseInt(price * 100);
+        console.log(amount);
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: 'usd',
+            payment_method_types: ['card'],
+          });
+
+          res.send({
+            clientSecret: paymentIntent.client_secret
+          })
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
